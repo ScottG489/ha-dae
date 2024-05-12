225341416570850d45951472530051245048ef33
Scott Giminiani <scottg489@gmail.com>
2024-05-12 17:15:48 -0400

    Test same user account can't be configured twice

--- tests/test_config_flow.py
+++ tests/test_config_flow.py
@@ -41,6 +41,33 @@ async def test_flow_user(hass: HomeAssistant, mock_login_response: LoginResponse
     assert creds_result["result"].title == USERNAME
 
 
+async def test_flow_user_already_configured(hass: HomeAssistant, mock_login_response: LoginResponse) -> None:
+    """Test full user setup flow."""
+    init_result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    with patch(
+            "custom_components.dae.config_flow.DaeClient.login",
+            new=mock_login_response,
+    ):
+        await hass.config_entries.flow.async_configure(
+            init_result["flow_id"], user_input=USER_INPUT_CREDS
+        )
+
+    init_result2 = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    with patch(
+            "custom_components.dae.config_flow.DaeClient.login",
+            new=mock_login_response,
+    ):
+        creds_result = await hass.config_entries.flow.async_configure(
+            init_result2["flow_id"], user_input=USER_INPUT_CREDS
+        )
+
+    assert creds_result["type"] == FlowResultType.ABORT
+    assert creds_result["reason"] == "already_configured"
+
 async def test_flow_user_fail(hass: HomeAssistant, mock_login_response_fail: LoginResponse) -> None:
     """Test user setup flow failure."""
     init_result = await hass.config_entries.flow.async_init(
